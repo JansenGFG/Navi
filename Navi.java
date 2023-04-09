6dded2a4faa2a9e12dae925057e1b8b46e5c4027
Jan <jan.gaertner3110@gmail.com>
2023-04-09 20:13:59 +0200

    l

---
+++ Navi.java
@@ -0,0 +1,9 @@
+public class Navi{
+    //http://graphonline.ru/en/?graph=zrScXpFEqPxzRwff
+    private int[][] matrix;
+    private String[] orte;
+    
+    private void start(){
+        
+    }
+}
